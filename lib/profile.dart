30008604273d83d68f2939d24847a07af6495342
Aqikol <jaisyahfaith@gmail.com>
2025-10-22 14:20:12 +0700

    Belajar Row Colum

---
+++ lib/profile.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: const Text(
+          'My Profile',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello World',
+              style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[],
+                ),
+                Column(
+                  children: <Widget>[],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
